02b640a0be5df8eaf636d17f233366fea0a2d34a
Matt Olson <52805964+Bigmo1575@users.noreply.github.com>
2021-02-08 12:30:38 -0600

    Delete js directory
    pasting this to the main branch I am pushing

--- js/designer.js
+++
@@ -1,6 +0,0 @@
-var c = document.getElementById('designArea');
-var ctx = c.getContext('2d');
-
-ctx.font = '50px Arial';
-ctx.textAlign = 'center';
-ctx.fillText('Coming Soon', c.width / 2, c.height / 2);
