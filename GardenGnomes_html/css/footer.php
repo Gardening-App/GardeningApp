2d3d32083f6f1b1f03e875bdb41c17344021bc37
Matt Olson <52805964+Bigmo1575@users.noreply.github.com>
2021-02-20 19:38:43 -0600

    Add files via upload

---
+++ GardenGnomes_html/css/footer.php
@@ -0,0 +1,49 @@
+<footer>
+	<img src="images/gnome.webp">
+	<nav>
+		<a href="about.html">About</a>
+		<a href="layout.html">Layout</a>
+		<a href="shop.html">Shop</a>
+		<a href="social.html">Social</a>
+	</nav>
+</footer>
+
+
+
+
+<style type="text/css">
+footer {
+	position: fixed;
+	bottom: 0px; 
+	left: 0px;
+	background-color: rgb(142, 199, 85);
+	width: 100%;
+	height: 50px;
+	
+}
+
+footer img{
+	width: auto;
+	height: 40px;
+	float: left;
+	position: relative;
+	left: 10px;
+	top: 5px
+}
+
+footer nav{
+	position: relative;
+	bottom: -15px;
+	left: 15px;
+}
+
+footer nav a{
+	padding-left: 15px;
+	color: rgba(255, 255, 255, 0.97);
+	text-decoration: none;
+}
+
+footer nav a:hover{
+	color: aqua;
+}
+</style>
