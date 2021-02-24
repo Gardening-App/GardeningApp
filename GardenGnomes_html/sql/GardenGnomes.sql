01b7157b0bcc7ec2d6c131e06fec34d5d1d33f5e
codiss <57338574+codiss@users.noreply.github.com>
2021-02-23 19:47:07 -0600

    Add files via upload

--- GardenGnomes_html/sql/GardenGnomes.sql
+++ GardenGnomes_html/sql/GardenGnomes.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: gardengnomes
@@ -123,3 +124,130 @@ CREATE TABLE `user` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2021-02-14 18:17:59
+=======
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gardengnomes
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `layout`
+--
+
+DROP TABLE IF EXISTS `layout`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `layout` (
+  `layoutID` int NOT NULL AUTO_INCREMENT,
+  `user_userID` int NOT NULL,
+  `Shape_shapeID` int NOT NULL,
+  PRIMARY KEY (`layoutID`),
+  KEY `fk_Layout_user1_idx` (`user_userID`),
+  KEY `fk_Layout_Shape1_idx` (`Shape_shapeID`),
+  CONSTRAINT `fk_Layout_Shape1` FOREIGN KEY (`Shape_shapeID`) REFERENCES `shape` (`shapeID`),
+  CONSTRAINT `fk_Layout_user1` FOREIGN KEY (`user_userID`) REFERENCES `user` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `shape`
+--
+
+DROP TABLE IF EXISTS `shape`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shape` (
+  `shapeID` int NOT NULL AUTO_INCREMENT,
+  `x1` int NOT NULL,
+  `y1` int NOT NULL,
+  `x2` int NOT NULL,
+  `y2` int NOT NULL,
+  PRIMARY KEY (`shapeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `shop`
+--
+
+DROP TABLE IF EXISTS `shop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shop` (
+  `shopID` int NOT NULL AUTO_INCREMENT,
+  `item` varchar(45) NOT NULL,
+  `discription` varchar(160) DEFAULT NULL,
+  `price` decimal(7,2) NOT NULL,
+  `picture` blob,
+  `link` varchar(2083) DEFAULT NULL,
+  `user_userID` int NOT NULL,
+  PRIMARY KEY (`shopID`),
+  KEY `fk_Shop_user1_idx` (`user_userID`),
+  CONSTRAINT `fk_Shop_user1` FOREIGN KEY (`user_userID`) REFERENCES `user` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `social`
+--
+
+DROP TABLE IF EXISTS `social`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `social` (
+  `socialID` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `comment` varchar(160) DEFAULT NULL,
+  `user_userID` int NOT NULL,
+  PRIMARY KEY (`socialID`),
+  KEY `fk_social_user1_idx` (`user_userID`),
+  CONSTRAINT `fk_social_user1` FOREIGN KEY (`user_userID`) REFERENCES `user` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping events for database 'gardengnomes'
+--
+
+--
+-- Dumping routines for database 'gardengnomes'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-14 18:17:59
+>>>>>>> Cody
