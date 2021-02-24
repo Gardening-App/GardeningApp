5edefeff5022d3f9af8b2d4d184b698323a973f2
Matt Olson <52805964+Bigmo1575@users.noreply.github.com>
2021-02-23 20:02:04 -0600

    Update about.js

--- GardenGnomes_html/Js/about.js
+++ GardenGnomes_html/Js/about.js
@@ -11,10 +11,35 @@ right: 'dayGridMonth,timeGridWeek,timeGridDay'
 },
 events: [
   {
-    title: 'example',
-  start: '2021-02-21' 
+    title: 'Bean Day',
+  start: '2021-01-06' 
+  },
+  {
+    title: 'Houseplant Appreciation Day',
+  start: '2021-01-10' 
+  },
+  {
+    title: 'National Seed Swap Day',
+  start: '2021-01-' 
   },
  
+  
+  {
+    title: 'Peach Blossom Day',
+  start: '2021-03-03' 
+  },
+  {
+    title: 'Johnny AppleSeed Day',
+  start: '2021-03-11' 
+  },
+  {
+    title: 'Plant a Flower Day',
+  start: '2021-03-12' 
+  },
+  {
+    title: 'Pecan Day',
+  start: '2021-03-25' 
+  },
 
 
 ]
