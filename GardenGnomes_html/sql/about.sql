a9fb0327a038c277f51f9b055d58d1b346f2afe8
Matt Olson <52805964+Bigmo1575@users.noreply.github.com>
2021-03-01 17:42:43 -0600

    Add files via upload

---
+++ GardenGnomes_html/sql/about.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `shacalender`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shacalendere` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(225) NOT NULL,
+  `start_event` datetime NOT NULL,
+  `end_event` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shape`
+--
+
+LOCK TABLES `shacalender` WRITE;
+/*!40000 ALTER TABLE `shape` DISABLE KEYS */;
+INSERT INTO `shacalender` 
+/*!40000 ALTER TABLE `shape` ENABLE KEYS */;
+UNLOCK TABLES;
