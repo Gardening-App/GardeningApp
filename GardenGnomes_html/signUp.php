15240361be9a3e0e71c6355776516a5aab186ddd
codiss <57338574+codiss@users.noreply.github.com>
2021-03-01 23:40:33 -0600

    Update signUp.php

--- GardenGnomes_html/signUp.php
+++ GardenGnomes_html/signUp.php
@@ -14,12 +14,57 @@
 <div class="content-wrapper">	
 <body>
 	<?php
-include("css/header.php"); 
+	if (!session_id()) {
+		session_start();
+	}
+	require("dbConnect.php");
+	require("dbfunctions.php");
+	require("callQuery.php");
+	include("css/header.php"); 
+
+
 ?> 
 
 	<div id="login">
 		<p> Sign Up </p>
 		
+
+<?php 
+$success = false;
+
+
+	if (isset($_POST['signup'])) {
+
+		// check for username
+		$query = "SELECT * FROM user
+					WHERE username = '" . $_POST['username'] . "'";
+		$errorMessage = "Error signing up";
+		$userNameRes = callQuery($pdo, $query, $errorMessage);
+
+		if ($userNameRes->fetch()) {
+			echo('Names taken, sorry.');
+		} else {
+			$sql = "INSERT INTO user (username, password) VALUES (?, ?)";
+
+            $pdo->beginTransaction();
+            $preppedSql = $pdo->prepare($sql);
+            $preppedSql->execute([$_POST['username'], $_POST['password']]);
+            $pdo->commit();
+
+			$success = true;
+			unset($_POST['signup']);
+			echo('Thanks for signing up');
+			$_SESSION['loggedIn'] = true;
+			$_SESSION['userID'] = getLastUserID($pdo);
+		}
+	}
+
+
+	// Display form if unsuccesful or haven't tried sign up
+
+	if (!$success) {
+
+?> 
 		<form action="" method = "post">
 		</form>
 
@@ -27,18 +72,23 @@ include("css/header.php");
 		<form action="" method = "post">
 			<div id="user">
 				<label  for="username">Username:</label>
-				<input name ="username">
+				<input name ="username" id = "username">
 			</div>
 			<div id="pass">
 				<label for ="password">Password:</label>
-				<input type = 'password' name ="password">
+				<input type = 'password' name ="password" id = "password">
 			</div>
 			<div id="confirm">
 				<label for ="password">Confirm Password:</label>
-				<input type = 'password' name ="password">
+				<input type = 'password' name ="password" id = "passwordConfirm">
 			</div>
-			<input class="submit" type="submit" name="login", value = "Sign Up">
+			<input class="submit" type="submit" name="signup" id = "submit", value = "Sign Up">
 		</form>
+
+
+		<?php
+	}
+	?> 
 
 			
 		
@@ -48,6 +98,8 @@ include("css/header.php");
 		<?php
 include("css/footer.php"); 
 ?> 
+<script src = "Js/jquery-3.5.1.min.js"></script>
+	<script src = 'Js/signup.js'></script>
 </body>
 </div>
 </html>
