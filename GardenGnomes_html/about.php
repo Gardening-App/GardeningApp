e593035e7cd52bedbbebebefd82937ebe0f3391c
Dylan Espie <74982313+despie22@users.noreply.github.com>
2021-03-03 12:27:15 -0600

    Updated about pictures

--- GardenGnomes_html/about.php
+++ GardenGnomes_html/about.php
@@ -1,24 +1,25 @@
 <?php
-	session_start();
+session_start();
 
 ?>
 
 <html>
+
 <head>
 
 
-    <link rel="stylesheet" href="css/about.css">
-    
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto+Slab&family=Yellowtail&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/about.css">
+
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto+Slab&family=Yellowtail&display=swap" rel="stylesheet">
 
 
 
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
@@ -31,169 +32,180 @@
 
 
 <div class="page-container">
-<div class="content-wrapper">
-<body>
+  <div class="content-wrapper">
 
-    <?php
-        include("css/header.php"); 
-     ?> 
+    <body>
 
-<          <figure class=figurePic>
-                <p><img class=scaled src="images/shop.jpeg" alt="Shop">
-                <figcaption>Shop out store</figcaption>
-            </figure>
+      <?php
+      include("css/header.php");
+      ?>
 
-            <figure class=figurePic>
-                <p><img class=scaled src="images/grid.jpeg" alt="Plan Garden">
-                <figcaption>Plan your garden</figcaption>
-            </figure>
+      <div class=column>
+        <figure class=figurePic>
+          <p><img class=scaled src="images/shop.jpeg" alt="Shop">
+          <figcaption>Shop out store</figcaption>
+        </figure>
 
-            <figure class=figurePic>
-                <p><img class=scaled src="images/blog.jpeg" alt="Social Page">
-                <figcaption>View our social page</figcaption>
-            </figure>
+        <figure class=figurePic>
+          <p><img class=scaled src="images/grid.jpeg" alt="Plan Garden">
+          <figcaption>Plan your garden</figcaption>
+        </figure>
 
-            <div id="welcomeText">
-                <p id="welcome">
-                    Do you ever get frustrated planning the layout of your garden and you can't remember what you have planted and where?
-                    Thats why we created Garden Gnomes. A community for gardening enthusiasts to plan and track their gardens. If you are
-                    new to gardening this is also a great place to learn and be social to see what other users are doing.
-                </p>
+        <figure class=figurePic>
+          <p><img class=scaled src="images/blog.jpeg" alt="Social Page">
+          <figcaption>View our social page</figcaption>
+        </figure>
+      </div>
 
-                <p id="welcome2">
-                    Check out our fun and easy to use<a href="layout.html"> layout </a> feature to keep track of your garden.
-                    This feature allows you to pick what fruit you are going to plant and place it so you can see how it will align with
-                    your current set up.
-                </p>
-            </div>
-<br><br><br><br><br>
+      <div id="welcomeText">
+        <p id="welcome">
+          Do you ever get frustrated planning the layout of your garden and you can't remember what you have planted and where?
+          Thats why we created Garden Gnomes. A community for gardening enthusiasts to plan and track their gardens. If you are
+          new to gardening this is also a great place to learn and be social to see what other users are doing.
+        </p>
 
-<!-- Logged in false by default -->
-<script>
-var loggedIn = false;
+        <p id="welcome2">
+          Check out our fun and easy to use<a href="layout.html"> layout </a> feature to keep track of your garden.
+          This feature allows you to pick what fruit you are going to plant and place it so you can see how it will align with
+          your current set up.
+        </p>
+      </div>
+      <br><br><br><br><br>
 
-<?php
-if (isset($_SESSION['userID']) && $_SESSION['userID'] == 1)
-{
- ?>
-loggedIn = true;
-<?php }?> 
+      <!-- Logged in false by default -->
+      <script>
+        var loggedIn = false;
 
-$(document).ready(function() {
-  var calendar = $('#calendar').fullCalendar({
-   editable:true,
-   header:{
-    left:'prev,next today',
-    center:'title',
-    right:'month,agendaWeek,agendaDay'
-   },
-   events: 'load.php',
-   selectable:true,
-   selectHelper:true,
-   select: function(start, end, allDay)
-   {
+        <?php
+        if (isset($_SESSION['userID']) && $_SESSION['userID'] == 1) {
+        ?>
+          loggedIn = true;
+        <?php } ?>
 
-    // Kill functionality if not logged in
-    if (!loggedIn) {
-      return false;
-    }
+        $(document).ready(function() {
+          var calendar = $('#calendar').fullCalendar({
+            editable: true,
+            header: {
+              left: 'prev,next today',
+              center: 'title',
+              right: 'month,agendaWeek,agendaDay'
+            },
+            events: 'load.php',
+            selectable: true,
+            selectHelper: true,
+            select: function(start, end, allDay) {
 
-    var title = prompt("Enter Event Title");
-    if(title)
-    {
-     var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-     var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
-     $.ajax({
-      url:"insert.php",
-      type:"POST",
-      data:{title:title, start:start, end:end},
-      success:function()
-      {
-       calendar.fullCalendar('refetchEvents');
-       alert("Added Successfully");
+              // Kill functionality if not logged in
+              if (!loggedIn) {
+                return false;
               }
-            })
-          }
-        },
-        editable:true,
-        eventResize: function(event)
-        {
-          var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-          var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
 
-          var title= event.title;
-          var id = event.id;
+              var title = prompt("Enter Event Title");
+              if (title) {
+                var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+                var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+                $.ajax({
+                  url: "insert.php",
+                  type: "POST",
+                  data: {
+                    title: title,
+                    start: start,
+                    end: end
+                  },
+                  success: function() {
+                    calendar.fullCalendar('refetchEvents');
+                    alert("Added Successfully");
+                  }
+                })
+              }
+            },
+            editable: true,
+            eventResize: function(event) {
+              var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+              var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
 
-          $.ajax ({
-            url: "update.php",
-            type:  "POST",
-            data:{title:title, start:start, end:end, id:id},
+              var title = event.title;
+              var id = event.id;
 
-            success:function(){
-              calendar.fullCalendar('refetchEvents');
-              alert('Event Update');
-            }
-          })
-        },
+              $.ajax({
+                url: "update.php",
+                type: "POST",
+                data: {
+                  title: title,
+                  start: start,
+                  end: end,
+                  id: id
+                },
 
-        eventDrop:function(event) 
-        
-        {
-          var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-          var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-          var title = event.title;
-          var id = event.id;
-          $.ajax({
-           url:"update.php",
-           type:"POST",
-           data:{title:title, start:start, end:end, id:id},
-           success:function()
-           {
-            calendar.fullCalendar('refetchEvents');
-            alert("Event Updated");
-           }
+                success: function() {
+                  calendar.fullCalendar('refetchEvents');
+                  alert('Event Update');
+                }
+              })
+            },
+
+            eventDrop: function(event)
+
+            {
+              var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+              var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+              var title = event.title;
+              var id = event.id;
+              $.ajax({
+                url: "update.php",
+                type: "POST",
+                data: {
+                  title: title,
+                  start: start,
+                  end: end,
+                  id: id
+                },
+                success: function() {
+                  calendar.fullCalendar('refetchEvents');
+                  alert("Event Updated");
+                }
+              });
+            },
+
+            eventClick: function(event) {
+              if (confirm("Are you sure you want to remove this?")) {
+                var id = event.id;
+                $.ajax({
+                  url: "delete.php",
+                  type: "POST",
+                  data: {
+                    id: id
+                  },
+                  success: function() {
+                    calendar.fullCalendar('refetchEvents');
+                    alert("Calendar Event Removed");
+                  }
+                })
+
+              }
+            },
+
+
+
           });
-         },
 
-         eventClick:function(event)
-         {
-          if(confirm("Are you sure you want to remove this?"))
-          {
-            var id = event.id;
-            $.ajax({
-              url:"delete.php",
-              type:"POST",
-              data:{id:id},
-              success:function()
-              {
-                calendar.fullCalendar('refetchEvents');
-                alert("Calendar Event Removed");
-              }
-            })
+        });
+      </script>
+      <?php if (!isset($_SESSION['userID']) || $_SESSION['userID'] != 1) { ?>
+        <p>Please log in to take advantage of our calendar!</p>
+      <?php
+      }
+      ?>
 
-          }
-         },
+      <div id='calendar'></div>
+      <br>
+  </div>
 
+  <?php
 
-
-  });
-
-});</script>
-<?php if (!isset($_SESSION['userID']) || $_SESSION['userID'] != 1)
-{ ?>
-<p>Please log in to take advantage of our calendar!</p>
-<?php
-} 
-?>
-
-<div id='calendar'></div>
-<br>
-</div>
-
-<?php
-
-    include("css/footer.php"); 
-?> 
+  include("css/footer.php");
+  ?>
 </div>
 </body>
+
 </html>
