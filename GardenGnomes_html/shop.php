2e69432398a48cb5d0ac6eaa27829af166debbbc
Dylan Espie <74982313+despie22@users.noreply.github.com>
2021-03-02 19:57:49 -0600

    Updated shop page
    Shop page now pulls from database to show items

--- GardenGnomes_html/shop.php
+++ GardenGnomes_html/shop.php
@@ -1,77 +1,39 @@
+<?php
+date_default_timezone_set('America/Chicago');
+require 'shopItems.php';
+require 'sanitize.php';
+require 'callQuery.php';
+?>
+
 <html>
 
 <head>
 
-  <link rel="stylesheet" href="css/about.css">
-  <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/shop.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto+Slab&family=Yellowtail&display=swap" rel="stylesheet">
-  <title>Shop Page</title>
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/shop.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto+Slab&family=Yellowtail&display=swap" rel="stylesheet">
+    <title>Shop Page</title>
 </head>
 <?php
-include("css/header.php"); 
-?> 
+include("css/header.php");
+?>
 <h1>Shop</h1>
-<div id="pages">
-</div>
 
 <body>
 
-  <div class="row">
-    <!-- Column 1 -->
+    <!-- Displays each shop item listed on the database -->
     <div class="column">
-      <!-- Seeds -->
-      <figure>
-        <img src="images/shop/seeds.jpg">
-        <figcaption>40 seed veriety!</figcaption>
-        <figcaption><a class="buy" href="https://www.amazon.com/dp/B06XDT5MLT/ref=cm_sw_em_r_mt_dp_P2FYST9FNW4EAADP7ZQN?_encoding=UTF8&psc=1" target="_blank" rel="noopener noreferrer">Buy</a> - $24.99</figcaption>
-      </figure>
-      <!-- Lights -->
-      <figure>
-        <img src="images/shop/lights.jpg">
-        <figcaption>Lights - 12 pack</figcaption>
-        <figcaption><a class="buy" href="https://www.amazon.com/dp/B082MHLDQJ/ref=cm_sw_em_r_mt_dp_467HE82DGSJ06HSFABPH" target="_blank" rel="noopener noreferrer">Buy</a> - $20.49</figcaption>
-      </figure>
+        <?php setShopItems($pdo) ?>
     </div>
-    <!-- Column 2 -->
-    <div class="column">
-      <!-- Tool Set -->
-      <figure>
-        <img src="images/shop/toolset.jpg">
-        <figcaption>Tool Set</figcaption>
-        <figcaption><a class="buy" href="https://www.amazon.com/dp/B08C7HXJR9/ref=cm_sw_em_r_mt_dp_0PRMMRRJEFXDQEXV0EXK?_encoding=UTF8&psc=1" target="_blank" rel="noopener noreferrer">Buy</a> - $19.99</figcaption>
-      </figure>
-      <!-- Plant food -->
-      <figure>
-        <img src="images/shop/plantfood.jpg">
-        <figcaption>Plant Food</figcaption>
-        <figcaption><a class="buy" href="https://www.amazon.com/dp/B000F6XGZ0/ref=cm_sw_em_r_mt_dp_8NHNXNW4KQKJPRHD7YSV" target="_blank" rel="noopener noreferrer">Buy</a> - $4.59</figcaption>
-      </figure>
-    </div>
-    <!-- Column 3 -->
-    <div class="column">
-      <!-- Hose -->
-      <figure>
-        <img src="images/shop/hose.jpg">
-        <figcaption>Expandable Hose</figcaption>
-        <figcaption><a class="buy" href="https://www.amazon.com/dp/B083PZ87JR/ref=cm_sw_em_r_mt_dp_04X1M3E8XX1VRTE0C9HA?_encoding=UTF8&psc=1" target="_blank" rel="noopener noreferrer">Buy</a> - $28.86</figcaption>
-      </figure>
-      <!-- Garden Gnome -->
-      <figure>
-        <img id="buyGnome" src="images/shop/gardengnome.jpg">
-        <figcaption>Garden Gnome</figcaption>
-        <figcaption><a class="buy" href="https://www.amazon.com/dp/B004DFNDK2/ref=cm_sw_em_r_mt_dp_R87T6DFV5NJHQ2T4A72A?_encoding=UTF8&psc=1" target="_blank" rel="noopener noreferrer">Buy</a> - $16.83</figcaption>
-      </figure>
-    </div>
-  </div>
-   
-	<?php
-include("css/footer.php");
 
-?> 
+    <?php
+    include("css/footer.php");
+    ?>
+
 </body>
 
 </html>
